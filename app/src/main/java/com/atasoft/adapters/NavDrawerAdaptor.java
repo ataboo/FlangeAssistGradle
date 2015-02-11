cc572c9bb6e844fafc7507b3c7be58eb4ed4913e
ataboo <bmtoolbox@gmail.com>
2015-02-11 04:50:35 -0700

    removing gradle files

--- app/src/main/java/com/atasoft/adapters/NavDrawerAdaptor.java
+++ app/src/main/java/com/atasoft/adapters/NavDrawerAdaptor.java
@@ -1,8 +1,7 @@
 package com.atasoft.adapters;
 
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
+
 
 import com.atasoft.flangeassist.fragments.*;
 
